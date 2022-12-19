3c74d1e233e29450e0f15f8e7010c487f1ccf19c
Abdullah Alnuman <alnuman@akijvgltd.onmicrosoft.com>
2022-12-19 08:57:45 +0600

    create-a-button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Button from '../components/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className='text-3xl font-bold'>Hello Tailwin CSS</h1>
         <h1 className='text-3xl font-bold'>Demo Branch code</h1>
+        <Button />
       </main>
     </>
   )
